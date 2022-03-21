341c4f830049660e159afba49bb18f9a1307bb1f
Ishant Dahiya <ishantdahiya@gmail.com>
2022-03-21 07:59:39 +0530

    a few changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from accounts import views
 
 urlpatterns = [
     path('dj-rest-auth/google/', views.GoogleLogin.as_view(), name='google_login'),
-    
+    path('get-all-orders/', views.GetAllOrder.as_view(), name='get_all_orders'),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,14 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from django.http.response import JsonResponse
+from django.contrib.auth.models import User
+
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from accounts.models import ShopifyAccessToken
+from accounts.orders import Shopify
 
 import os
 
@@ -9,3 +17,15 @@ class GoogleLogin(SocialLoginView):
     callback_url = os.environ.get('GOOGLE_CALLBACK_URL')
     client_class = OAuth2Client
     
+class GetAllOrder(APIView):
+    permission_classes = [AllowAny]
+    def get(self ,request, *args, **kwargs):
+        
+        email = request.query_params.get('email', False)
+        if not email:
+            return JsonResponse({'error': 'email is required'}, status=400)
+        
+        user = User.objects.get(email=email)
+        access_token = ShopifyAccessToken.objects.filter(user=user).first()
+        s = Shopify(access_token)
+        return JsonResponse(s.all_orders(), status=200)
